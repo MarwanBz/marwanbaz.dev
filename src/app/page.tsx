7e41a96dd86f6cc8803487b6809ab4a7fa5d2061
MarwanBz <kaka.mer1998@gmail.com>
2024-12-31 21:26:22 +0300

    fix: fix build erorr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 // import { Button } from "@/components/ui/button";
-import ComingSoon from "@/components/components_coming-soon";
-// import Image from "next/image";
+
 
 export default function Home() {
   return (
