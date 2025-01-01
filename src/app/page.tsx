36a5a38e3025a3394a21f87ec6cc3e9afd2b5ea4
MarwanBz <kaka.mer1998@gmail.com>
2025-01-01 22:05:55 +0300

    feat(hero): create hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 // import { Button } from "@/components/ui/button";
 
+import Hero from "@/components/hero";
 
 export default function Home() {
   return (
     <div>
-     
+     <Hero />
     </div>
   );
 }
