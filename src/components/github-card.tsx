5b76ac809afb31d012516bc767d4fa34a00b2f35
MarwanBz <kaka.mer1998@gmail.com>
2025-01-04 20:11:09 +0300

    feat: create social cards

---
+++ src/components/github-card.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import { CustomCard } from "./custom-card"
+import { Github } from 'lucide-react'
+import { motion } from "framer-motion"
+
+interface GitHubLinkProps {
+  url: string
+}
+
+export function GitHubLink({ url }: GitHubLinkProps) {
+  return (
+    <CustomCard
+      className="p-8 aspect-square"
+      hover={true}
+      onClick={() => window.open(url, '_blank')}
+    >
+      <motion.div
+        className="w-full h-full flex items-center justify-center"
+        whileHover={{
+          scale: 1.2,
+          rotate: 360,
+          transition: { duration: 0.8, ease: "easeInOut" }
+        }}
+      >
+        <Github className="w-10 h-10 text-black dark:text-white" />
+      </motion.div>
+    </CustomCard>
+  )
+}
+
