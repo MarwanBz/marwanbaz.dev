2d5d4c57dfd54ec0052b62554b6fa300986ea666
MarwanBz <kaka.mer1998@gmail.com>
2025-01-15 21:51:35 +0300

    fix: fix build erorr

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 
 import type { Metadata } from "next";
 import Navbar from "@/components/navbar";
-import { PageTransition } from "@/components/page-transition";
+// import { PageTransition } from "@/components/page-transition";
 import { ThemeProvider } from "@/components/components_theme-provider";
 
 const geistSans = Geist({
