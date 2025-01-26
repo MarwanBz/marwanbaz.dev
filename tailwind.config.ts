d4039912050e3840c8be833a2c6b7ab6fbd1996c
MarwanBz <kaka.mer1998@gmail.com>
2025-01-26 11:35:04 +0300

    feat: create shine border component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -68,13 +68,25 @@ module.exports = {
 		  "pop-blob": {
 			"0%, 100%": { transform: "scale(1)" },
 			"50%": { transform: "scale(1.1)" },
-		  },
+			},
+		   shine: {
+          "0%": {
+            "background-position": "0% 0%",
+          },
+          "50%": {
+            "background-position": "100% 100%",
+          },
+          to: {
+            "background-position": "0% 0%",
+          },
+        },
 		},
 		animation: {
 		  "accordion-down": "accordion-down 0.2s ease-out",
 		  "accordion-up": "accordion-up 0.2s ease-out",
 		  "pop-blob": "pop-blob 7s infinite",
-		  'spin-slow': 'spin 8s linear infinite',
+			'spin-slow': 'spin 8s linear infinite',
+		  shine: "shine var(--duration) infinite linear",
 		},
 	  },
 	},
